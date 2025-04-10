d4529a881a68c309270031f8203408db23c0d1c3
Omer Lande <omerl@log-on.com>
2025-04-10 12:51:28 +0300

    fixed ul for meal items

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,5 +1,6 @@
 import { useEffect } from "react";
 import { useState } from "react";
+import MealItem from "./MealItem";
 
 export default function Meals() {
   const [loadedMeals, setLoadedMeals] = useState([]);
@@ -19,21 +20,10 @@ export default function Meals() {
     fetchMeals();
   }, []);
 
-  async function fetchMeals() {
-    const response = await fetch("http://localhost:3000/meals");
-
-    if (!response.ok) {
-      //...
-    }
-
-    const meals = await response.json();
-    setLoadedMeals(meals);
-  }
-
   return (
     <ul id="meals">
       {loadedMeals.map((meal) => (
-        <li key={meal.id}>{meal.name}</li>
+        <MealItem key={meal.id} meal={meal} />
       ))}
     </ul>
   );
